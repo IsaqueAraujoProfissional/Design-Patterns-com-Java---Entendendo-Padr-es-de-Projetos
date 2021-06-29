6900b9e98a6058a7a526cde7f43e8f37d7337c2f
IsaqueAraujoProfissional <isaquearaujo.profissional@gmail.com>
2021-06-29 14:40:50 -0300

    Inicando estudo de SOLID, primeiro principio SRP

---
+++ SOLID/SingleResponsibilityPrinciple/src/br/com/isaque/srp/Main.java
@@ -0,0 +1,10 @@
+package br.com.isaque.srp;
+
+public class Main {
+    //O objetivo do SRP é não ter uma classe inflada, ou seja, as responsabilidades são dividas.
+    public static void main(String[] args) {
+    }
+    public void listClient(){}
+    public void addClient(){}
+    public void updateClient(){}
+}
---
+++ SOLID/SingleResponsibilityPrinciple/src/br/com/isaque/srp/utility/Notify.java
@@ -0,0 +1,6 @@
+package br.com.isaque.srp.utility;
+
+public class Notify {
+    //Classe com um único objetivo e responsabilidade
+    public void notifyClient(){}
+}
