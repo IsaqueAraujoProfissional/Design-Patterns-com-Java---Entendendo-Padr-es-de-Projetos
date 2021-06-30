8d4bed85660885284fb997ddef4c382d84104f6e
IsaqueAraujoProfissional <isaquearaujo.profissional@gmail.com>
2021-06-30 17:47:40 -0300

    Design Pattern - Comportamental - Chain of Responsability

---
+++ DesignPatterns/Comportamental/ChainOfResponsability/src/br/com/isaque/chainofresponsability/middlewares/CheckPermissionMiddleware.java
@@ -0,0 +1,13 @@
+package br.com.isaque.chainofresponsability.middlewares;
+
+public class CheckPermissionMiddleware extends Middleware{
+    @Override
+    public boolean check(String email, String password) {
+        if(email.equals("master@hcode.com.br")){
+            System.out.println("Bem Vindo, Administrador!");
+            return true;
+        }
+        System.out.println("Bem vindo!");
+        return checkNext(email, password);
+    }
+}
---
+++ DesignPatterns/Comportamental/ChainOfResponsability/src/br/com/isaque/chainofresponsability/middlewares/CheckUserMiddleware.java
@@ -0,0 +1,24 @@
+package br.com.isaque.chainofresponsability.middlewares;
+
+import br.com.isaque.chainofresponsability.server.Server;
+
+public class CheckUserMiddleware extends Middleware{
+    private Server server;
+
+    public  CheckUserMiddleware(Server server){
+        this.server = server;
+    }
+
+    @Override
+    public boolean check(String email, String password) {
+        if(!server.hasEmail(email)){
+            System.out.println("E-mail Inválido!");
+            return false;
+        }
+        if(!server.isValidPassword(email, password)){
+            System.out.println("E-mail ou Senha Inválidos!");
+            return false;
+        }
+        return checkNext(email, password);
+    }
+}
